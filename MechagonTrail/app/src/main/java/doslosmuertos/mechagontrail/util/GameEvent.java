d8a6f6c5283ad200e21d708de8cd255ce42609d3
seadler <seadler@mtu.edu>
2015-03-27 00:03:30 -0400

    Made The event class
    There is now an event class

---
+++ MechagonTrail/app/src/main/java/doslosmuertos/mechagontrail/util/GameEvent.java
@@ -0,0 +1,19 @@
+package doslosmuertos.mechagontrail.util;
+
+/**
+ * Created by Evelyn on 3/26/2015.
+ */
+public class GameEvent {
+    private String description;
+    private int cashChange;
+    private int[] healthChange = new int[5];
+    private double[] hungerChange = new double[5];
+    private double[] fatigueChange = new double[5];
+    private boolean[] diseasedChange = new boolean[5];
+    private String[] diseaseChange = new String[5];
+    private int[] mechHealthChange = new int[5];
+
+
+
+
+}
