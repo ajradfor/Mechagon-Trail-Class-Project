9fdcdb5b53dd98683ccc18c615113e28fa360637
seadler <seadler@mtu.edu>
2015-04-08 22:08:39 -0400

    Event class
    Methods to the event class where added.

--- MechagonTrail/app/src/main/java/doslosmuertos/mechagontrail/util/GameState.java
+++ MechagonTrail/app/src/main/java/doslosmuertos/mechagontrail/util/GameState.java
@@ -8,6 +8,8 @@ public class GameState{
     public GameCharacter leftLeg;
     public GameCharacter rightLeg;
 
+    public String eventText;
+
     private GameMech mech;
 
     private double pace;
